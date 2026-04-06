5c858b58fb63218aa00285b1132db240d32b40fc
britneylu <BRL154@pitt.edu>
2026-04-06 14:26:03 -0400

    implemented todos

--- CommitCafe.java
+++ CommitCafe.java
@@ -1,13 +1,15 @@
 public class CommitCafe {
 
-    private static String barista = "Nadine"; // STUDENT_TODO_1A: Change name
+    private static String barista = "Britney \"The Brewer\""; // STUDENT_TODO_1A: Change name
     // STUDENT_TODO_1B: Add a nickname or title for the barista
 
     private static int cups = 0;
 
     public static void brew(String drink) {
         // STUDENT_TODO_2A: Implement - add 1 to cups & print a message
+	cups++;
         // STUDENT_TODO_2B: Add a second brew-related improvement
+	System.out.println(barista + " brewed a cup of " + drink);
     }
 
     public static void printSummary() {
